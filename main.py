5ef3f25053f076ddeba479f12d99f55742cc17c6
Chalasani Charan <2000030152@kluniversity.in>
2022-01-22 10:57:30 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,30 @@
+class Student:
+    college = "KL University"
+
+  def_init_(self,m1,m2,m3):
+    self.m1=m1
+    self.m2=m2
+    self.m3=m3
+
+def avg(self):
+    return(self.m1+self.m2+self.m3)/3
+
+    @classmethod
+    def info(cls):
+        return cls.college
+
+    s1=Student(86,97,59)
+    s2=Student(98,67,85)
+
+    @classmethod
+    def set(cls):
+        return cls.college=klef
+
+
+
+
+
+
+
+
+
